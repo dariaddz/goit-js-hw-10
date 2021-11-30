1cab35469d7e9366745b4ee3914bf9964563c92b
dariaddz <dariadz@gmail.com>
2021-11-30 16:47:01 +0200

    не работает Ошибка

--- src/index.js
+++ src/index.js
@@ -2,48 +2,42 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import countryCardTmplt from './templates/country-cards.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 // ------refs------------------
 const refs = {
   countryCardContainer: document.querySelector('.country-info'),
+  searchBox: document.getElementById('search-box'),
 };
+const DEBOUNCE_DELAY = 300;
+
+refs.searchBox.addEventListener('input', debounce(onSearchBoxInput, DEBOUNCE_DELAY));
 
 // -------------------------------
-let countryName = 'tu';
+// let countryName = 'tu';
 
-fetch(
-  `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`,
-)
-  .then(response => {
-    return response.json();
-  })
-  .then(renderCountryCard)
+// ----------- события по инпуту-----
+function onSearchBoxInput(e) {
+  // ----------------------принимаем массив стран и строим разметку----------
+  let countryName = e.target.value;
+  fetchCountries(countryName).then(renderCountryCard).catch(onSearchError);
 
-  .catch(error => {
-    console.log(error);
-  });
-
-function renderCountryCard(country) {
-  console.log(country);
-  const markup = countryCardTmplt(country);
-  refs.countryCardContainer.innerHTML = markup;
-
-  console.log(markup);
+  console.log(countryName);
 }
 
-// function resultCheck(country) {
-//   if (this.length > 10) {
-//     Notify.info('list is tooo long');
-//   } else if ((country.length = 1)) {
-//     Notify.info('will create big card');
-//   } else {
-//     Notify.info('will create list ');
-//   }
-// }
+//------получаем страны ----------
+function fetchCountries(countryName) {
+  const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
+  return fetch(url).then(response => {
+    return response.json();
+  });
+}
+//-----СТРОИМ РАЗМЕТКУ---------
+function renderCountryCard(country) {
+  // console.log(country);
+  const markup = countryCardTmplt(country);
+  refs.countryCardContainer.innerHTML = markup;
+}
 
-// function oneCountry() {
-//   console.log();
-// }
-
-// function listOfCountries() {
-//   console.log();
-// }
+function onSearchError(error) {
+  Notify.failure('No countries found');
+}
