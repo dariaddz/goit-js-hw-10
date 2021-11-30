65bffa4171e422f745b919272027df8d29160f2a
dariaddz <dariadz@gmail.com>
2021-11-30 15:11:49 +0200

    markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import countryCardTmplt from './templates/country-cards.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// ------refs------------------
+const refs = {
+  countryCardContainer: document.querySelector('.country-info'),
+};
 
-const DEBOUNCE_DELAY = 300;
+// -------------------------------
+let countryName = 'tu';
+
+fetch(
+  `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`,
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(renderCountryCard)
+
+  .catch(error => {
+    console.log(error);
+  });
+
+function renderCountryCard(country) {
+  console.log(country);
+  const markup = countryCardTmplt(country);
+  refs.countryCardContainer.innerHTML = markup;
+
+  console.log(markup);
+}
+
+// function resultCheck(country) {
+//   if (this.length > 10) {
+//     Notify.info('list is tooo long');
+//   } else if ((country.length = 1)) {
+//     Notify.info('will create big card');
+//   } else {
+//     Notify.info('will create list ');
+//   }
+// }
+
+// function oneCountry() {
+//   console.log();
+// }
+
+// function listOfCountries() {
+//   console.log();
+// }
